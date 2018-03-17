e92064da669d74bddfd7fb1b08bff6e1d7c5a7b5
Tung Thanh Thai <snowstorm2965@gmail.com>
2018-03-17 18:25:47 +0100

    commit 1: use AsyncTask

--- app/src/main/java/com/example/unipdlibrarytestapp/MainActivity.java
+++ app/src/main/java/com/example/unipdlibrarytestapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.unipdlibrarytestapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void download(View view) {
+        new DownloadTask().execute();
+    }
+
+    private class DownloadTask extends AsyncTask<Void, Void, String> {
+
+        /*@Override
+        protected void onPreExecute() {
+
+        }*/
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result = "MESSAGE FROM THE UNDERWORLD";
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(result);
+        }
     }
 }
